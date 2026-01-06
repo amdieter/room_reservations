c6acba1064923ceb78e8f9bda0bb2c1c1def0cf8
Aaron Dieter <aarondieter714@gmail.com>
2026-01-05 22:01:07 -0600

    Implement google api to access google sheet

--- room_reservations.js
+++ room_reservations.js
@@ -1,6 +1,36 @@
 // command to run local preview
 // npx http-server /Users/aaron/Documents/javascript/room_reservations/room_reservations.html -o -p 9999
 
+const { google } = require("googleapis");
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: "credentials.json",
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+async function readSheet() {
+  const client = await auth.getClient();
+
+  const sheets = google.sheets({
+    version: "v4",
+    auth: client,
+  });
+
+  const spreadsheetId = "13CdEbddbIDDHdCVU9IvpHQzSB4lJroJ36Riufl5uvTk";
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range: "Studio (1)!A3:H67",
+  });
+
+  console.log(response.data.values);
+}
+
+readSheet();
+
+// const timeHeading = document.querySelector("#curr_time");
+// timeHeading.textContent = "Practice Rooms Available for " + getTime() + ":";
+
 // get time of day in 15 min increments to get timeslot
 function getTime() {
   let time = "";
@@ -16,13 +46,13 @@ function getTime() {
   }
 
   if (mins < 15) {
-    newMins = 0;
+    newMins = "00";
   } else if (mins < 30) {
-    newMins = 15;
+    newMins = "15";
   } else if (mins < 45) {
-    newMins = 30;
+    newMins = "30";
   } else {
-    newMins = 45;
+    newMins = "45";
   }
 
   if (am) {
@@ -33,5 +63,3 @@ function getTime() {
 
   return time;
 }
-
-console.log(getTime());
